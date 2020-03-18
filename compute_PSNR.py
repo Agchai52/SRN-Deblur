cc02f9e6d573387ebb3435e9744746f0ee835fca
Agchai52 <shuangzhang0809@gmail.com>
2020-03-18 19:02:49 -0400

    test for aided

--- compute_PSNR.py
+++ compute_PSNR.py
@@ -6,16 +6,20 @@ from skimage.measure import compare_ssim as ssim
 
 
 def psnr(img1, img2):
-    mse = np.mean( (img1 - img2) ** 2 )
+    mse = np.mean((img1 - img2) ** 2)
     if mse == 0:
         return 100
     PIXEL_MAX = 255.0
     return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
 
 
-deblu_root = './test_all_noise_res' #_
-sharp_root = './test_full_noise' #_
+deblu_root = './test_aided'  #
+sharp_root = './dataset/AidedDeblur/test/'  # _
 
+f_test = open("./dataset/AidedDeblur/test_instance_names.txt", "r")
+test_data = f_test.readlines()
+sharp_list = [line.rstrip() for line in test_data]
+f_test.close()
 deblu_list = os.listdir(deblu_root)
 sharp_list = os.listdir(sharp_root)
 sharp_list = sorted(sharp_list, key=str.lower)
@@ -25,29 +29,33 @@ num_imgs = len(deblu_list)
 PSNR_all = []
 SSIM_all = []
 
-for n, item in enumerate(sharp_list):
+for item in sharp_list:
     if not item.startswith('.'):
 
-        name_sharp = sharp_list[n]
-        name_deblu = name_sharp
+        name_sharp = item[-6:]
+        name_deblu = 'test_' + name_sharp + '_blur_err.png'
 
         path_deblu = os.path.join(deblu_root, name_deblu)
-        path_sharp = os.path.join(sharp_root, name_sharp)
+        path_sharp = item + '_ref.png'
 
         img_deblu = cv2.imread(path_deblu, cv2.IMREAD_COLOR).astype(np.float)
         img_sharp = cv2.imread(path_sharp, cv2.IMREAD_COLOR).astype(np.float)
 
-        _, ws, _ = img_sharp.shape
-        img_sharp = img_sharp[:, int(ws/2):ws]
+        print(img_deblu.shape)
+        img_deblu = cv2.resize(img_deblu, (1280, 720))
 
-        #cv2.imshow('sharp', img_sharp/255)
-        #cv2.imshow('deblur', img_deblu/255)
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
+        print(img_deblu.shape)
+        # cv2.imshow('sharp', img_sharp/255)
+        # cv2.imshow('deblur', img_deblu/255)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
         psnr_n = psnr(img_deblu, img_sharp)
-        ssim_n = ssim(img_deblu / 255, img_sharp / 255, gaussian_weights=True, multichannel=True,use_sample_covariance=False)
+        ssim_n = ssim(img_deblu / 255, img_sharp / 255, gaussian_weights=True, multichannel=True,
+                      use_sample_covariance=False)
 
+        if item[-2:] == '01':
+            print("Test Image {}, PSNR = {}, SSIM = {}".format(name_sharp, psnr_n, ssim_n))
         PSNR_all.append(psnr_n)
         SSIM_all.append(ssim_n)
     else:
@@ -55,8 +63,7 @@ for n, item in enumerate(sharp_list):
 
 PSNR = np.mean(PSNR_all)
 SSIM = np.mean(SSIM_all)
-#print(PSNR_all)
-#print(SSIM_all)
+# print(PSNR_all)
+# print(SSIM_all)
 print(PSNR)
 print(SSIM)
-
