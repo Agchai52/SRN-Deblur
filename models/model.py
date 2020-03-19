f1698f4ac6ac02223affda627eee202b83e428a6
Agchai52 <shuangzhang0809@gmail.com>
2020-03-19 17:56:20 -0400

    detect bugs

--- models/model.py
+++ models/model.py
@@ -284,7 +284,7 @@ class DEBLUR(object):
             blur = scipy.misc.imread(imgName + '_blur_err.png')
             imgName = 'test_' + imgName[-6:] + '_blur_err.png'
             h, w, c = blur.shape
-            print(max(blur))
+            print(np.max(blur))
             #blur = blur[:, :int(w / 2)]
             #_, blur, _ = np.split(blur, 3, axis=1)
             #w = blur.shape[1]
