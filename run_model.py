11e4bdb9115a939b823327a9de0bdccfce55e775
Agchai52 <shuangzhang0809@gmail.com>
2020-09-24 17:27:11 -0400

    SRN: test general images

--- run_model.py
+++ run_model.py
@@ -15,7 +15,7 @@ def parse_args():
     parser.add_argument('--epoch', help='training epoch number', type=int, default=500)
     parser.add_argument('--lr', type=float, default=1e-4, dest='learning_rate', help='initial learning rate')
     parser.add_argument('--gpu', dest='gpu_id', type=str, default='0', help='use gpu or cpu')
-    parser.add_argument('--height', type=int, default=1365,
+    parser.add_argument('--height', type=int, default=1364,
                         help='height for the tensorflow placeholder, should be multiples of 16')
     parser.add_argument('--width', type=int, default=2048,
                         help='width for the tensorflow placeholder, should be multiple of 16 for 3 scales')
